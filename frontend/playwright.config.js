edfd459aad3cdc65edc9cd4e505c03ca649eba8b
Justin Abraham <abrahamjustin24@gmail.com>
2024-12-16 17:40:52 -0500

    fixed local run

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -4,7 +4,7 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './e2e', // Only look for tests in the e2e directory
   use: {
-    baseURL: 'http://host.docker.internal:3000', // Adjust the base URL as needed
+    baseURL: 'http://localhost:3000', // Adjust the base URL as needed
     headless: true,
   },
 });
--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -4,13 +4,17 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ element, ...rest }) => {
   const token = localStorage.getItem('token');
 
+  console.log("we got this token: ", token);
   // Function to check if the token is expired
   const isTokenExpired = (token) => {
     if (!token) return true; // No token means it's expired or doesn't exist
     const decoded = JSON.parse(atob(token.split('.')[1])); // Decode the token to get the payload
     const expiration = decoded.exp * 1000; // Convert expiration to milliseconds
+    console.log("we got this expiration: ", expiration, Date.now());
     return Date.now() > expiration; // Compare current time with expiration time
   };
+
+  console.log("is the token expired, ", isTokenExpired(token));
 
   const isAuthenticated = token && !isTokenExpired(token);
 
